d2ecac9f4ee26d21b4e84e4112d6335cc9ca93e6
Brett Cantrell <cantrell.brett8892@gmail.com>
2022-12-27 20:56:52 -0600

    Created parent classes

---
+++ game.py
@@ -0,0 +1,33 @@
+# scissor cuts paper
+# paper cover rock
+# rock crushes lizard
+# lizard poisons spock
+# spock smashes scissors
+# scissors decapitates lizard
+# lizard eats paper
+# paper disproves spock
+# spock vaporizes rock
+# rock crushes scissors
+
+class Game:
+    def __init__(self):
+        
+        pass
+
+
+    def start_game(self):
+        self.game_instructions
+
+    def game_instructions(self):
+        print("Here are the instructions for the game!")
+        print()
+        print("scissor cuts paper")
+        print("paper covers rock")
+        print("rock crushes lizard")
+        print("lizard poisons spock")
+        print("spock smashes scissors")
+        print("scissors decaptitates lizard")
+        print("lizard eats paper")
+        print("paper disproves spock")
+        print("spock vaporizes rock")
+        print("rock crushes scissors")
